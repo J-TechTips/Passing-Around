18ff6b80056ba919d5ebcf57f5b0aa80f00978ab
Jonathan <jtecktips@gmail.com>
2018-06-15 12:52:02 -0700

    Added text to be passed to child

---
+++ src/app/ParentComponent/parent-component.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'parent-component',
+  templateUrl: './parent-component.component.html',
+  styleUrls: ['./parent-component.component.css']
+})
+export class ParentComponent implements OnInit {
+  childTitle: string = "text passed to child";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
